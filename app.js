951f256ca79f09acc9f28448ada405d1c74a0ff6
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-24 00:37:16 +0100

    Esercizio completato.

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // TEST
-console.log('Ciao a tutti!');
+console.log('Hello Vue!');
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        containerClass: 'container',
+        titleClass: 'title',
+        message: 'Io dopo aver scoperto quanto ti semplifica la vita Vue.js:',
+        imageUrl: 'img/ragequit.gif',
+        imageClass: 'gif',
+    }
+});
+
+console.log(app);
